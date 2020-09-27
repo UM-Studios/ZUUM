e8d9d655c2a5e8426edd6056f02de1567c0f0e44
TingYi Wu <tingyi.wu1@gmail.com>
2020-09-27 11:06:06 -0700

    changed mac appdata to use home directory

--- appdata.py
+++ appdata.py
@@ -2,12 +2,13 @@ APPNAME = "ZUUM"
 import sys
 from os import path, environ, makedirs
 if sys.platform == 'darwin':
-    from AppKit import NSSearchPathForDirectoriesInDomains
+    ad = path.expanduser(path.join("~", "." + APPNAME))
+    #from AppKit import NSSearchPathForDirectoriesInDomains
     # http://developer.apple.com/DOCUMENTATION/Cocoa/Reference/Foundation/Miscellaneous/Foundation_Functions/Reference/reference.html#//apple_ref/c/func/NSSearchPathForDirectoriesInDomains
     # NSApplicationSupportDirectory = 14
     # NSUserDomainMask = 1
     # True for expanding the tilde into a fully qualified path
-    ad = path.join(NSSearchPathForDirectoriesInDomains(14, 1, True)[0], APPNAME)
+    #ad = path.join(NSSearchPathForDirectoriesInDomains(14, 1, True)[0], APPNAME)
 elif sys.platform == 'win32':
     ad = path.join(environ['APPDATA'], APPNAME)
 else:
