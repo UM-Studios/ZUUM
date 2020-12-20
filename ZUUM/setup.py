c8bd47947415e56a6388a7131912994b711603e0
Gnit 1 <gnit1@gnits-mac.lan>
2020-12-19 20:57:21 -0800

    hopefully fixed compile script for mac

--- ZUUM/setup.py
+++ ZUUM/setup.py
@@ -1,5 +1,6 @@
 import sys
 from cx_Freeze import setup, Executable
+from os import path
 
 
 
@@ -7,25 +8,37 @@ if sys.platform == 'win32':
     includefiles = ['addstartup.ps1', 'AddException.bat', 'static/', 'templates/']
     base = 'Win32GUI'
     location = 'build\\win\\Zuum\\'
+    build_exe_options = {
+        "packages": ["os", "sys"], 
+        "excludes": ["tkinter", "PyQt4.QtSql",
+                    "scipy.lib.lapack.flapack",
+                    "PyQt4.QtNetwork",
+                    "PyQt4.QtScript",
+                    "numpy.core._dotblas",
+                    "PyQt5"],
+        "includes": ["sqlalchemy", "html.parser", "sqlalchemy.dialects.sqlite", "sqlalchemy.sql.default_comparator", "APSched", "setuptools", "jinja2.ext"],
+        'build_exe': location,
+        'include_files': includefiles
+    }
 else:
     includefiles = ['static/', 'templates/'] # hi colin add extra shell scripts or smth here
     base = None
     location = 'build/mac/Zuum'
-
-
-build_exe_options = {
-    "packages": ["os", "sys"], 
-    "excludes": ["tkinter", "PyQt4.QtSql",
-                "scipy.lib.lapack.flapack",
-                "PyQt4.QtNetwork",
-                "PyQt4.QtScript",
-                "numpy.core._dotblas",
-                "PyQt5"],
-    "includes": ["sqlalchemy", "html.parser", "sqlalchemy.dialects.sqlite", "sqlalchemy.sql.default_comparator", "APSched", "setuptools", "jinja2.ext"],
-    'build_exe': location,
-    'include_files': includefiles
-}
-
+    build_exe_options = {
+        "packages": ["os", "sys", "_sysconfigdata__darwin_darwin"], 
+        "excludes": ["tkinter", "PyQt4.QtSql",
+                    "scipy.lib.lapack.flapack",
+                    "PyQt4.QtNetwork",
+                    "PyQt4.QtScript",
+                    "numpy.core._dotblas",
+                    "PyQt5"],
+        "includes": ["sqlalchemy", "sqlalchemy.dialects.sqlite", "jinja2.ext", "sqlalchemy.sql.default_comparator", "html.parser"],
+        # 'build_exe': location,
+        'include_files': includefiles
+    }
+    bdist_mac_options = {
+        
+    }
 
 setup(  name = "ZUUM",
         version = "0.1",
@@ -35,13 +48,13 @@ setup(  name = "ZUUM",
             Executable(
                 script="taskrunner.py",
                 targetName="ZuumScheduler",
-                icon="static\\img\\zmlogo.ico",
+                icon=path.join('static','img','zmlogo.ico'),
                 base = base
             ),
             Executable(
                 script="main.py",
                 targetName="ZuumManager",
-                icon="static\\img\\zmlogo.ico",
+                icon=path.join('static','img','zmlogo.ico'),
                 base = base
             )
         ])
