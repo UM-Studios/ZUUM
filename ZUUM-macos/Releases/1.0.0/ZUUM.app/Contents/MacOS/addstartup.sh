8125f8e5cbee1bb031eedf1ef67e0b93f532ca50
Colin Zhao <colinzhao777@gmail.com>
2021-01-17 01:42:44 -0800

    v1.0.0 installer

---
+++ ZUUM-macos/Releases/1.0.0/ZUUM.app/Contents/MacOS/addstartup.sh
@@ -0,0 +1,2 @@
+pw="$(osascript -e 'Tell application "System Events" to display dialog "Password:" default answer "" with hidden answer' -e 'text returned of result' 2>/dev/null)" && echo "$pw" | sudo -S cp com.ZUUM.scheduler.plist ~/Library/LaunchAgents
+launchctl start com.ZUUM.scheduler
